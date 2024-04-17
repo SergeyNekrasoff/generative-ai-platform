0619c16a0f293239379f7f5efdb6042d1f6810bf
judixel <judixel2015@ya.ru>
2024-04-17 12:01:41 +0400

    add modes nav

--- src/widgets/navigation/ui/Navigation.vue
+++ src/widgets/navigation/ui/Navigation.vue
@@ -1,87 +1,168 @@
 <template>
-  <aside class="navigation light:bg-gray_dark_1">
-    <div class="flex items-start justify-between w-full">
-      <ul class="navigation__list">
-        <li
-          class="flex items-center justify-start mt-8 w-full hover:bg-divider_dark_2 py-2 px-4 rounded-md"
-        >
-          <HomeIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/" class="text-lg font-bold pt-1 w-full">Home</router-link>
-        </li>
-        <li class="mb-16 mt-4">
-          <hr class="text-divider_dark_2" />
-        </li>
-        <li class="text-xs py-2 text-text_dark_3">Workspace</li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <DocumentTextIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/projects" class="text-lg font-bold pt-1 w-full">Projects</router-link>
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <RectangleStackIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/templates" class="text-lg font-bold pt-1 w-full">Templates</router-link>
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <ChatBubbleBottomCenterTextIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/chat" class="text-lg font-bold pt-1 w-full">Chat</router-link>
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <PencilSquareIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/chat" class="text-lg font-bold pt-1 w-full">Editor</router-link>
-        </li>
-        <li class="mb-4 mt-4">
-          <hr class="text-divider_dark_2" />
-        </li>
-        <!-- <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <DocumentMagnifyingGlassIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/history" class="text-lg font-bold pt-1 w-full">History</router-link>
-        </li> -->
-      </ul>
-    </div>
-    <div class="flex items-start justify-between w-full">
-      <ul class="navigation__list">
-        <li class="text-xs py-2 text-text_dark_3">Settings</li>
-        <li class="flex items-center justify-start w-full py-2 px-2 rounded-md">
-          <BookOpenIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          242 words used
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <Cog8ToothIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <router-link to="/settings" class="pt-1 w-full">Settings</router-link>
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md mb-10"
-        >
-          <PlusIcon class="h-5 w-5 mr-2 text-text_dark_2" />
-          <button type="button" class="pt-1" @click="openModal">Invite members</button>
-        </li>
-        <li
-          class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
-        >
-          <span class="flex items-center justify-center h-8 w-10 rounded-full bg-purple mr-3">
-            J
-          </span>
-          <router-link to="/account" class="pt-1 w-full">Judixel89</router-link>
-          <ChevronRightIcon class="h-5 w-5" />
-        </li>
-      </ul>
-    </div>
-  </aside>
+  <Transition name="fade" mode="out-in">
+    <aside v-if="!collapsed" class="navigation light:bg-gray_dark_1">
+      <div
+        class="absolute left-[325px] top-24 hidden -translate-x-14 rotate-90 transform cursor-pointer flex-row items-center justify-center rounded-t-md bg-black_mute px-3 py-1 text-sm text-white shadow-overlay duration-300 lg:flex z-50 border-r border-t border-l border-divider_dark_2 hover:bg-black_soft"
+        @click="toggle"
+      >
+        <ArrowLeftIcon class="h-5 w-5 text-text_dark_2 -rotate-90 transform" />
+      </div>
+      <div class="flex items-start justify-between w-full">
+        <ul class="navigation__list">
+          <li
+            class="flex items-center justify-start mt-8 w-full hover:bg-divider_dark_2 py-2 px-4 rounded-md"
+          >
+            <HomeIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/" class="text-lg font-bold pt-1 w-full">Home</router-link>
+          </li>
+          <li class="mb-16 mt-4">
+            <hr class="text-divider_dark_2" />
+          </li>
+          <li class="text-xs py-2 text-text_dark_3">Workspace</li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <DocumentTextIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/projects" class="text-lg font-bold pt-1 w-full">Projects</router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <RectangleStackIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/templates" class="text-lg font-bold pt-1 w-full"
+              >Templates</router-link
+            >
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <ChatBubbleBottomCenterTextIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/chat" class="text-lg font-bold pt-1 w-full">Chat</router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <PencilSquareIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/chat" class="text-lg font-bold pt-1 w-full">Editor</router-link>
+          </li>
+          <li class="mb-4 mt-4">
+            <hr class="text-divider_dark_2" />
+          </li>
+          <!-- <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <DocumentMagnifyingGlassIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/history" class="text-lg font-bold pt-1 w-full">History</router-link>
+          </li> -->
+        </ul>
+      </div>
+      <div class="flex items-start justify-between w-full">
+        <ul class="navigation__list">
+          <li class="text-xs py-2 text-text_dark_3">Settings</li>
+          <li class="flex items-center justify-start w-full py-2 px-2 rounded-md">
+            <BookOpenIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            242 words used
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <Cog8ToothIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <router-link to="/settings" class="pt-1 w-full">Settings</router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md mb-10"
+          >
+            <PlusIcon class="h-5 w-5 mr-2 text-text_dark_2" />
+            <button type="button" class="pt-1" @click="openModal">Invite members</button>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 py-2 px-2 rounded-md"
+          >
+            <span class="flex items-center justify-center h-8 w-10 rounded-full bg-purple mr-3">
+              J
+            </span>
+            <router-link to="/account" class="pt-1 w-full">Judixel89</router-link>
+            <ChevronRightIcon class="h-5 w-5" />
+          </li>
+        </ul>
+      </div>
+    </aside>
+    <aside v-else class="navigation navigation--collapsed light:bg-gray_dark_1">
+      <div
+        class="absolute left-[105px] top-24 hidden -translate-x-14 rotate-90 transform cursor-pointer flex-row items-center justify-center rounded-t-md bg-black_mute px-3 py-1 text-sm text-white shadow-overlay duration-300 lg:flex z-50 border-r border-t border-l border-divider_dark_2 hover:bg-black_soft"
+        @click="toggle"
+      >
+        <ArrowRightIcon class="h-5 w-5 text-text_dark_2 -rotate-90 transform" />
+      </div>
+      <div class="flex items-start justify-between w-full">
+        <ul class="navigation__list">
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-10"
+          >
+            <router-link to="/">
+              <HomeIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-2"
+          >
+            <router-link to="/projects">
+              <DocumentTextIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-2"
+          >
+            <router-link to="/templates">
+              <RectangleStackIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-2"
+          >
+            <router-link to="/chat">
+              <ChatBubbleBottomCenterTextIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+          <li class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md">
+            <router-link to="/chat">
+              <PencilSquareIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="flex items-start justify-between w-full">
+        <ul class="navigation__list">
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-2"
+          >
+            <router-link to="/settings">
+              <Cog8ToothIcon class="h-6 w-6 text-text_dark_2" />
+            </router-link>
+          </li>
+          <li
+            class="flex items-center justify-start w-full hover:bg-divider_dark_2 p-2 rounded-md mb-10"
+          >
+            <button type="button" @click="openModal">
+              <PlusIcon class="h-6 w-6 text-text_dark_2" />
+            </button>
+          </li>
+          <li class="flex items-center justify-center w-full p-2 rounded-md">
+            <router-link to="/account">
+              <span class="flex items-center justify-center h-8 w-8 rounded-full bg-purple">
+                J
+              </span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  </Transition>
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
+import type { Ref } from 'vue'
 
 import { HomeIcon } from '@heroicons/vue/24/solid'
 import { DocumentTextIcon } from '@heroicons/vue/24/solid'
@@ -93,6 +174,8 @@ import { ChevronRightIcon } from '@heroicons/vue/24/solid'
 import { DocumentMagnifyingGlassIcon } from '@heroicons/vue/24/solid'
 import { ChatBubbleBottomCenterTextIcon } from '@heroicons/vue/24/solid'
 import { PencilSquareIcon } from '@heroicons/vue/24/solid'
+import { ArrowLeftIcon } from '@heroicons/vue/24/solid'
+import { ArrowRightIcon } from '@heroicons/vue/24/solid'
 
 import { useModal } from '@/shared/ui/modal/model/useModal'
 
@@ -100,9 +183,13 @@ const InviteModal = defineAsyncComponent(() => import('@/shared/ui/modal/invite/
 
 const modal = useModal()
 
+const collapsed: Ref<boolean> = ref(false)
+
 const openModal = async () => {
   modal.show({ component: InviteModal })
 }
+
+const toggle = () => (collapsed.value = !collapsed.value)
 </script>
 
 <style lang="scss">
